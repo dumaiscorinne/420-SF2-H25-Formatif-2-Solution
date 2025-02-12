0ef974a2fac357f64b73e3ad0e9affc5785e0a74
Corinne Dumais <Corinne.Dumais@cegeplimoilou.ca>
2025-02-12 16:04:51 -0500

    Ajout d'un message de présentation du programme

--- Formatif2/src/Main.java
+++ Formatif2/src/Main.java
@@ -6,5 +6,7 @@ public class Main {
         Cercle c1 = new Cercle(Couleur.VERT, 10);
         Rectangle r = new Rectangle(Couleur.BLEU, 5, 8);
         Triangle t = new Triangle(Couleur.JAUNE, 7, 7, 7);
+
+        System.out.println("Ceci est un programme qui gère des formes");
     }
 }
