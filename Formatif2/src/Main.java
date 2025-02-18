cd717456a08b91ba83470ddd46b479a853baba17
corinne.dumais <corinne.dumais@cegeplimoilou.ca>
2025-02-18 12:13:02 -0500

    Modification du Main

--- Formatif2/src/Main.java
+++ Formatif2/src/Main.java
@@ -4,10 +4,6 @@ import exceptions.GrandeurFormeException;
 public class Main {
     public static void main(String[] args) {
         Cercle c1 = new Cercle(Couleur.VERT, 10);
-        Rectangle r = new Rectangle(Couleur.BLEU, 5, 8);
-        Triangle t = new Triangle(Couleur.JAUNE, 7, 7, 7);
-
         System.out.println("Ceci est un programme qui gère des formes !!");
-        System.out.println("Ceci est une modification!");
     }
 }
